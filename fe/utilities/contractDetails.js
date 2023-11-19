29b1ad8d94d3df1c4a1c74cb729759baa833f440
veroni-d20 <veronishwetha.23it@licet.ac.in>
2023-11-19 21:03:21 +0530

    minor fix

--- fe/utilities/contractDetails.js
+++ fe/utilities/contractDetails.js
@@ -2,12 +2,14 @@ import LIQUIDITY_POOL from "../../contracts/deployments/mumbai/LiquidityPool.jso
 import ATTESTATION from "../../contracts/deployments/mumbai/Attestation.json";
 import CARBON_CREDITS from "../../contracts/deployments/mumbai/CarbonCredits.json";
 import CARBONIUM_TOKEN from "../../contracts/deployments/mumbai/CarboniumToken.json";
+// import EAS_CARBONIUM from "../../contracts/deployments/mumbai/EasCarbonium.json";
 
 const DEPLOYED_CONTRACTS = {
   LIQUIDITY_POOL,
   ATTESTATION,
   CARBON_CREDITS,
   CARBONIUM_TOKEN,
+  // EAS_CARBONIUM,
 };
 
 export default DEPLOYED_CONTRACTS;
