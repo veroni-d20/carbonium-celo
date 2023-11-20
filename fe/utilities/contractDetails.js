894a87a4099c685a41daa22fe46d76831882fb49
veroni-d20 <veronishwetha.23it@licet.ac.in>
2023-11-20 12:17:40 +0530

    celo network

--- fe/utilities/contractDetails.js
+++ fe/utilities/contractDetails.js
@@ -1,15 +1,15 @@
-import LIQUIDITY_POOL from "../../contracts/deployments/mumbai/LiquidityPool.json";
-import ATTESTATION from "../../contracts/deployments/mumbai/Attestation.json";
-import CARBON_CREDITS from "../../contracts/deployments/mumbai/CarbonCredits.json";
-import CARBONIUM_TOKEN from "../../contracts/deployments/mumbai/CarboniumToken.json";
-// import EAS_CARBONIUM from "../../contracts/deployments/celoAlfajores/EasCarbonium.json";
+import LIQUIDITY_POOL from "../../contracts/deployments/celoAlfajores/LiquidityPool.json";
+import ATTESTATION from "../../contracts/deployments/celoAlfajores/Attestation.json";
+import CARBON_CREDITS from "../../contracts/deployments/celoAlfajores/CarbonCredits.json";
+import CARBONIUM_TOKEN from "../../contracts/deployments/celoAlfajores/CarboniumToken.json";
+import EAS_CARBONIUM from "../../contracts/deployments/celoAlfajores/EasCarbonium.json";
 
 const DEPLOYED_CONTRACTS = {
   LIQUIDITY_POOL,
   ATTESTATION,
   CARBON_CREDITS,
   CARBONIUM_TOKEN,
-  // EAS_CARBONIUM
+  EAS_CARBONIUM,
 };
 
 export default DEPLOYED_CONTRACTS;
